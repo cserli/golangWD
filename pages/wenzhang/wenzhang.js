42781f9fee83d693337ebf1be03cbe581f1b237b
cserli <1528884535@qq.com>
2018-05-17 14:25:49 +0800

    77

--- pages/wenzhang/wenzhang.js
+++ pages/wenzhang/wenzhang.js
@@ -10,20 +10,13 @@ Page({
     this.setData({
       focus: true
     })
- 
   },
+  // 问答数据操作
   bindTextAreaBlur: function (e) {
     console.log("--------" + e.detail.value + app.globalData.g_userInfo.nickName + app.globalData.g_userInfo.avatarUrl)
     // post gRPC server
     wx.request({
-      url: "https://wx.golang.ltd:7878/TJData",
-      method: 'GET',
-      data: {
-        Protocol: "1",
-        Protocol2: "2", // 发表吐槽
-        nickName: app.globalData.g_userInfo.nickName,
-        avatarUrl: app.globalData.g_userInfo.avatarUrl,
-        param: e.detail.value
+
       },
       header: {
         "Content-Type": "application/json"
