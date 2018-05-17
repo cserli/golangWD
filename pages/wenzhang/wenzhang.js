5b5a16bf58a9671039a8afd813700d968d206363
cserli <1528884535@qq.com>
2018-05-17 14:23:17 +0800

    555

--- pages/wenzhang/wenzhang.js
+++ pages/wenzhang/wenzhang.js
@@ -1,33 +1,41 @@
-
+var app = getApp();
 
 Page({
   data: {
     height: 20,
     focus: false,
-    datalist:"" // 数据
+    datalist: "" // 数据
   },
   bindButtonTap: function (e) {
     this.setData({
       focus: true
-    }),
+    })
+ 
+  },
+  bindTextAreaBlur: function (e) {
+    console.log("--------" + e.detail.value + app.globalData.g_userInfo.nickName + app.globalData.g_userInfo.avatarUrl)
     // post gRPC server
-    wx.navigateTo({
-      // wx.request({
-      // url: "https://wx.golang.ltd:7878/TJData",
-      url: "../setting/setting",
+    wx.request({
+      url: "https://wx.golang.ltd:7878/TJData",
+      method: 'GET',
+      data: {
+        Protocol: "1",
+        Protocol2: "2", // 发表吐槽
+        nickName: app.globalData.g_userInfo.nickName,
+        avatarUrl: app.globalData.g_userInfo.avatarUrl,
+        param: e.detail.value
+      },
       header: {
         "Content-Type": "application/json"
       },
       success: function (res) {
-        //console.log(res.data)
+        // 更新数据缓存
+        console.log(res.data)
       },
       fail: function (err) {
         console.log(err)
       },
       complete: function () { }
     })
-  },
-  bindTextAreaBlur: function (e) {
-    console.log(e.detail.value)
   }
 })
